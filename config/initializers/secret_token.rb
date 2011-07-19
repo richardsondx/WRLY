74a5feafd0213d90363915924645adf8eb6af501
RichardsonDx <richardsondx@gmail.com>
2011-07-19 06:55:15 -0400

    MicropostAnswers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WRLY::Application.config.secret_token = '330a16f753f7992807dc8e837b994c17d7c9badac6228b2f1617c549dd51adfe60721a05ea0d87a4a64811652508fbf9976c581a96eea592f1effc8de884ecf9'
---
+++ db/migrate/20110719100536_add_micropost_id_column.rb
@@ -0,0 +1,7 @@
+class AddMicropostIdColumn < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
